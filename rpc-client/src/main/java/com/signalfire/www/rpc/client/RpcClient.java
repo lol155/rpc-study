81812d73b3ede8c64bc03206b57c810a764048ea
lol155 <1055455968@qq.com>
2017-12-30 23:27:58 +0800

    rpcclient

--- rpc-client/src/main/java/com/signalfire/www/rpc/client/RpcClient.java
+++ rpc-client/src/main/java/com/signalfire/www/rpc/client/RpcClient.java
@@ -1,8 +1,14 @@
 package com.signalfire.www.rpc.client;
 
+import com.signalfire.www.rpc.common.RpcDecoder;
+import com.signalfire.www.rpc.common.RpcEncoder;
+import com.signalfire.www.rpc.common.RpcRequest;
 import com.signalfire.www.rpc.common.RpcResponse;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,4 +54,40 @@ public class RpcClient extends SimpleChannelInboundHandler<RpcResponse>{
         LOGGER.error("client caught exception", cause);
         ctx.close();
     }
+
+
+    public RpcResponse send(RpcRequest request) throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast(new RpcEncoder(RpcRequest.class))  //OUT-1
+                                    .addLast(new RpcDecoder(RpcResponse.class)) //IN -1
+                                    .addLast(RpcClient.this);                   //IN -2
+                        }
+                    }).option(ChannelOption.SO_KEEPALIVE, true);
+            // 链接服务器
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            //将request对象写入outbundle处理后发出（即RpcEncoder编码器）
+            future.channel().writeAndFlush(request).sync();
+
+            // 用线程等待的方式决定是否关闭连接
+            // 其意义是：先在此阻塞，等待获取到服务端的返回后，被唤醒，从而关闭网络连接
+            synchronized (obj) {
+                obj.wait();
+            }
+            if (response != null) {
+                future.channel().closeFuture().sync();
+            }
+            return response;
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+
 }
